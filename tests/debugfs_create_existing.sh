d77cab78da11dc855deaa61aed95ebbd1d3b7f15
Valerie Aurora <val@versity.com>
2025-03-03 14:47:03 +0100

    Add create tests
    Test creating a file and creating an already existing file.
    
    Signed-off-by: Valerie Aurora <val@versity.com>

---
+++ tests/debugfs_create_existing.sh
@@ -0,0 +1,12 @@
+
+#
+# test that creating an existing file fails
+#
+
+t_require_commands ngnfs-cli
+
+echo "== creating an existing file with debugfs"
+echo -e "mkfs\ncreate file\nstat file\ncreate file\nstat file\nquit\n" | ngnfs-cli debugfs $T_CLIENT_ADDRS -t "$T_TMPDIR/trace" | sed 's/time: [0-9].*/time: [REDACTED]/' | sed 's/ino: [0-9].*/ino: [REDACTED]/'
+[ $? == 0 ] || t_fail "debugfs command failed"
+
+t_pass
