295eb4b6344a91dd36b0ac5fd3ae35cbf15886f6
Valerie Aurora <val@versity.com>
2025-03-03 14:58:48 +0100

    Add readdir test
    Test readdir of directories.
    
    Signed-off-by: Valerie Aurora <val@versity.com>

---
+++ tests/debugfs_readdir.sh
@@ -0,0 +1,12 @@
+
+#
+# test readdir
+#
+
+t_require_commands ngnfs-cli
+
+echo "== reading directory with debugfs"
+echo -e "mkfs\nreaddir\ncreate file\nmkdir dir\nreaddir\ncd dir\nreaddir\nquit\n" | ngnfs-cli debugfs $T_CLIENT_ADDRS -t "$T_TMPDIR/trace" | sed 's/ino: [0-9].*/ino: [REDACTED]/'
+[ $? == 0 ] || t_fail "debugfs command failed"
+
+t_pass
