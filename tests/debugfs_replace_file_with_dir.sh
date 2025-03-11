c35018faed5deee4d1241e2081a013bac842f2e6
Valerie Aurora <val@versity.com>
2025-03-11 20:17:23 +0100

    Add replace file with dir test
    Signed-off-by: Valerie Aurora <val@versity.com>

---
+++ tests/debugfs_replace_file_with_dir.sh
@@ -0,0 +1,11 @@
+
+#
+# test renaming a dir over an existing file
+#
+
+t_require_commands ngnfs-cli
+
+echo "== renaming a directory over a file with debugfs"
+echo -e "mkfs\ncreate target\nmkdir dir\ncd dir\nmkdir target\nrename target\nstat target\ncd ..\nstat target\nquit\n" | ngnfs-cli debugfs $T_CLIENT_ADDRS -t "$T_TMPDIR/trace" | sed 's/time: [0-9].*/time: [REDACTED]/' | sed 's/ino: [0-9].*/ino: [REDACTED]/'
+
+t_pass
