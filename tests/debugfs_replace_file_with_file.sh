4083061cd5c27e9ef1a492a754b5c3284636dacd
Valerie Aurora <val@versity.com>
2025-03-11 20:08:51 +0100

    Add rename over an existing file
    Signed-off-by: Valerie Aurora <val@versity.com>

---
+++ tests/debugfs_replace_file_with_file.sh
@@ -0,0 +1,12 @@
+
+#
+# test renaming a file over another file
+#
+
+t_require_commands ngnfs-cli
+
+echo "== renaming a file over a file with debugfs"
+echo -e "mkfs\nmkdir dir\ncd dir\ncreate file\nrename file\nstat file\ncd ..\nstat file\nquit\n" | ngnfs-cli debugfs $T_CLIENT_ADDRS -t "$T_TMPDIR/trace" | sed 's/time: [0-9].*/time: [REDACTED]/' | sed 's/ino: [0-9].*/ino: [REDACTED]/'
+[ $? == 0 ] || t_fail "debugfs command failed"
+
+t_pass
