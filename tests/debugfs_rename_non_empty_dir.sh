b9f8e1f2e7e2b7e99317b3f926b7ecf2b22880e4
Valerie Aurora <val@versity.com>
2025-04-03 21:25:35 +0200

    Add test to rename a non-empty dir
    Signed-off-by: Valerie Aurora <val@versity.com>

---
+++ tests/debugfs_rename_non_empty_dir.sh
@@ -0,0 +1,12 @@
+
+#
+# test renaming a non-empty dir
+#
+
+t_require_commands ngnfs-cli
+
+echo "== renaming a non-empty directory with debugfs"
+echo -e "mkfs\nmkdir dir\ncd dir\nmkdir dir2\ncd dir2\ncreate file\ncd ..\nrename dir2\nstat dir2\ncd ..\nstat dir2\nquit\n" | ngnfs-cli debugfs $T_CLIENT_ADDRS -t "$T_TMPDIR/trace" | sed 's/time: [0-9].*/time: [REDACTED]/' | sed 's/ino: [0-9].*/ino: [REDACTED]/'
+[ $? == 0 ] || t_fail "debugfs command failed"
+
+t_pass
