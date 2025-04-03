e1993a39e8c3e4d126270a503c5b2baf638d7b9d
Valerie Aurora <val@versity.com>
2025-04-03 22:27:56 +0200

    Add a test for a too long extended attribute.
    Signed-off-by: Valerie Aurora <val@versity.com>

---
+++ tests/debugfs_xattr_set_too_long.sh
@@ -0,0 +1,14 @@
+
+#
+# test that setting an xattr that is too long fails
+#
+
+t_require_commands ngnfs-cli
+
+echo "== setting an xattr that is too long with debugfs"
+xattr_value=`head -c 503 < /dev/zero | tr '\0' 'a'`
+debugfs_cmd="mkfs\nsetxattr . a ${xattr_value}\ngetxattr . a\nquit\n"
+echo -e ${debugfs_cmd} | ngnfs-cli debugfs $T_CLIENT_ADDRS -t "$T_TMPDIR/trace"
+[ $? == 0 ] || t_fail "debugfs command failed"
+
+t_pass
