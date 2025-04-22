4c6f9fa42dbdbca77a55ccefd5643d3af08740ca
Valerie Aurora <val@versity.com>
2025-04-22 17:19:33 +0200

    Update max xattr name/value after removing unnecessary padding
    Signed-off-by: Valerie Aurora <val@versity.com>

--- tests/debugfs_xattr_set_too_long.sh
+++ tests/debugfs_xattr_set_too_long.sh
@@ -6,7 +6,7 @@
 t_require_commands ngnfs-cli
 
 echo "== setting an xattr that is too long with debugfs"
-xattr_value=`head -c 503 < /dev/zero | tr '\0' 'a'`
+xattr_value=`head -c 508 < /dev/zero | tr '\0' 'a'`
 debugfs_cmd="mkfs\nsetxattr . a ${xattr_value}\ngetxattr . a\nquit\n"
 echo -e ${debugfs_cmd} | ngnfs-cli debugfs $T_CLIENT_ADDRS -t "$T_TMPDIR/trace"
 [ $? == 0 ] || t_fail "debugfs command failed"
