e1ac9bcdf6e8bcca066d5fa4aa1741103ed29521
Valerie Aurora <val@versity.com>
2025-04-29 16:16:45 +0200

    Add a listxattr to some xattr tests to catch undeleted attrs
    Signed-off-by: Valerie Aurora <val@versity.com>

--- tests/debugfs_xattr_remove.sh
+++ tests/debugfs_xattr_remove.sh
@@ -6,7 +6,7 @@
 t_require_commands ngnfs-cli
 
 echo "== removing an xattr with debugfs"
-echo -e "mkfs\nsetxattr . name value\ngetxattr . name\nremovexattr . name\ngetxattr . name\nquit\n" | ngnfs-cli debugfs $T_CLIENT_ADDRS -t "$T_TMPDIR/trace"
+echo -e "mkfs\nsetxattr . name value\ngetxattr . name\nremovexattr . name\ngetxattr . name\nlistxattr .\nquit\n" | ngnfs-cli debugfs $T_CLIENT_ADDRS -t "$T_TMPDIR/trace"
 [ $? == 0 ] || t_fail "debugfs command failed"
 
 t_pass
--- tests/debugfs_xattr_set_replace.sh
+++ tests/debugfs_xattr_set_replace.sh
@@ -6,7 +6,7 @@
 t_require_commands ngnfs-cli
 
 echo "== setting an xattr with replace flag with debugfs"
-echo -e "mkfs\nsetxattr . name value replace\ngetxattr . name\nsetxattr . name value create\ngetxattr . name\nsetxattr . name value2 replace\ngetxattr . name\nquit\n" | ngnfs-cli debugfs $T_CLIENT_ADDRS -t "$T_TMPDIR/trace"
+echo -e "mkfs\nsetxattr . name value replace\ngetxattr . name\nsetxattr . name value create\ngetxattr . name\nsetxattr . name value2 replace\ngetxattr . name\nlistxattr .\nquit\n" | ngnfs-cli debugfs $T_CLIENT_ADDRS -t "$T_TMPDIR/trace"
 [ $? == 0 ] || t_fail "debugfs command failed"
 
 t_pass
