c4227e610ed75f64f06724c40ddb0b12592edc79
Valerie Aurora <val@versity.com>
2025-05-01 19:49:36 +0200

    Add tests that force a split and merge of btree blocks
    Signed-off-by: Valerie Aurora <val@versity.com>

---
+++ tests/debugfs_xattr_force_split.sh
@@ -0,0 +1,13 @@
+
+#
+# test that xattrs in btree will split
+#
+
+t_require_commands ngnfs-cli
+
+echo "== forcing a split of btrees with debugfs"
+xattr_value=`head -c 500 < /dev/zero | tr '\0' 'a'`
+echo -e "mkfs\nsetxattr . 1 $xattr_value\nsetxattr . 2 $xattr_value\nsetxattr . 3 $xattr_value\nsetxattr . 4 $xattr_value\nsetxattr . 5 $xattr_value\nsetxattr . 6 $xattr_value\nsetxattr . 7 $xattr_value\nsetxattr . 8 $xattr_value\nsetxattr . 9 $xattr_value\nsetxattr . 10 $xattr_value\nlistxattr .\nquit\n" | ngnfs-cli debugfs $T_CLIENT_ADDRS -t "$T_TMPDIR/trace"
+[ $? == 0 ] || t_fail "debugfs command failed"
+
+t_pass
